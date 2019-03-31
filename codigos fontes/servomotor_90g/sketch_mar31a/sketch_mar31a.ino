59b8ac93207b9611c8241f0af1d2e03f468d0b93
vanderfranco <vanderfranco@gmail.com>
2019-03-31 14:59:16 -0300

    servo_motor_apresetacao

--- codigos fontes/servomotor_90g/sketch_mar31a/sketch_mar31a.ino
+++ codigos fontes/servomotor_90g/sketch_mar31a/sketch_mar31a.ino
@@ -14,15 +14,16 @@ void setup ()
  
 void loop()
 {
-  for(pos = 0; pos < 90; pos++)
+  for(pos = 0; pos < 180; pos++)
   {
     s.write(pos);
   delay(15);
   }
-  delay(1000);
-  for(pos = 90; pos >= 0; pos--)
+  delay(2000);
+  for(pos = 180; pos >= 0; pos--)
   {
     s.write(pos);
     delay(15);
   }
+  delay(2000);
 }
